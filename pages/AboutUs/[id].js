91335fe42a182ac4f9a5acc6034535ca7a8dec4f
pheonix2121 <bobbypanwar73@gmail.com>
2023-11-11 00:58:15 +0530

    task-4

--- pages/AboutUs/[id].js
+++ pages/AboutUs/[id].js
@@ -1,30 +1,23 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
-const details = [
-  { id: 1, name: "Yash", role: "Senior Developer" },
-
-  { id: 2, name: "Vaibhav", role: "Backend Developer" },
-
-  { id: 3, name: "Suresh", role: "Frontend Developer" },
-];
-
 const Developer = () => {
-  const [developer, setDeveloper] = useState({});
-  const { id } = useRouter().query;
-  useEffect(() => {
-    setDeveloper(details.find((detail) => detail.id === +id));
-  }, [id]);
-  console.log(developer);
-  return (
-    <>
-      {developer ? (
-        <h1>
-          {developer.name} {developer.role}
-        </h1>
-      ) : (
-        <h1>Developer doesn't exist.</h1>
-      )}
+    const [developer, setDeveloper] = useState({});
+    const { id } = useRouter().query;
+  
+    useEffect(() => {
+      setDeveloper(details.find((detail) => detail.id === +id));
+    }, [id]);
+  
+  
+    return (
+      <>
+        {developer ? (
+          <div>
+            <h1>
+            {developer.name} {developer.role}
+            </h1>
+          </div>
+        ) : (
+          <h1>Developer doesn't exist.</h1>
+        )}
     </>
   );
 };
--- pages/Index.js
+++ pages/Index.js
@@ -1,5 +1,25 @@
-const HomePage = () => {
-    return <h1>This is HomePage</h1>;
-  };
-  
-  export default HomePage;
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+
+const AboutUs = () => {
+  return (
+    <div>
+      {details.map((person) => {
+        return (
+          <h1 key={person.id}>
+            <Link href={`/AboutUs/${person.id}`}>{person.name}</Link>
+          </h1>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AboutUs;
