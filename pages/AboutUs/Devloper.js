5f96d325e5050c9d27922c58e04af78143c42606
pheonix2121 <bobbypanwar73@gmail.com>
2023-11-11 00:27:36 +0530

    task-2

---
+++ pages/AboutUs/Devloper.js
@@ -0,0 +1,10 @@
+const DeveloperPage = () => {
+    return (
+      <div>
+        <h1>Developer</h1>
+        <p>My Name Is bobby</p>
+      </div>
+    );
+  };
+  
+  export default DeveloperPage;
