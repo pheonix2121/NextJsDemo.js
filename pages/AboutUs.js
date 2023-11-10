d96190e0403b8b18429bab09be6cda159c980ba0
pheonix2121 <bobbypanwar73@gmail.com>
2023-11-11 00:22:10 +0530

    task-1

---
+++ pages/AboutUs.js
@@ -0,0 +1,5 @@
+const AboutUsPage = () => {
+    return <h1>AboutUs</h1>;
+  };
+  
+  export default AboutUsPage;
---
+++ pages/Index.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+    return <h1>This is HomePage</h1>;
+  };
+  
+  export default HomePage;
